441419b30b4c0c3b8627ebd76153c3987f52215f
jsrav1 <rainerviguini1@hotmail.com>
2021-02-10 12:52:34 -0300

    update all project

--- scripts.js
+++ scripts.js
@@ -1,20 +1,17 @@
-const modalOverlay = document.querySelector ('.modal-overlay')
+const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
-    card.addEventListener ("click", function(){
-        const imgId = card.getAttribute("id")
-        const description1 = card.querySelector("a").innerHTML
-        const description2 = card.querySelector("p").innerHTML
+    card.addEventListener("click", function() {
+        let modalImg = card.getAttribute("id");
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector("img").src = `${imgId}.png`
-        modalOverlay.querySelector("a").innerHTML = `${description1}`
-        modalOverlay.querySelector("p").innerHTML = `${description2}`
-        
+        modalOverlay.querySelector("img").src = `assets/${modalImg}.png`;
+        modalOverlay.querySelector('h4').innerHTML = card.querySelector('h4').textContent
+        modalOverlay.querySelector('p').innerHTML = card.querySelector('p').textContent
     })
 }
 
-document.querySelector(".close-modal").addEventListener("click", function(){
+document.querySelector(".close-modal").addEventListener("click", function() {
     modalOverlay.classList.remove("active")
-    modalOverlay.querySelector("img").src = ""
+    modalOverlay.querySelector("").src = ""
 })
